60fffa2b47dd62e037f740558c3552fb92236207
arther <artheremail@163.com>
2018-01-18 16:36:58 +0800

    创建项目

---
+++ MyProject/MyProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyProject
+//
+//  Created by neoby on 2018/1/18.
+//  Copyright © 2018年 arther. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
